2eef6063ac8d7098ccbdb44371e01e2b8f1202be
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-04 17:01:25 +0100

    creating CategoryElement component

---
+++ components/CategoryElement.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
+import styles from './CategoryElement.module.scss';
+
+interface Props {
+    img: string;
+    categoryName: string
+}
+
+const CategoryElement = ({ img, categoryName }: Props) => {
+
+    return (
+        <Card
+            className={styles.categoryCard}
+            sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="400"
+                    image={img}
+                    alt={categoryName}
+                />
+                <CardContent
+                    className={styles.cardContent}
+                >
+                    <Typography gutterBottom variant="h5" component="div">
+                        {categoryName}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default CategoryElement;
