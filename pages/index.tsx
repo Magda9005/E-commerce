71baee0fc5f23ed3690a176ec5073febc9afdc1a
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-07 10:27:01 +0100

    replacing pages file by index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import MainPage from "../components/MainPage";
+import Head from "next/head";
+
+
+const WelcomePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Skin Therapy </title>
+      </Head>
+      <MainPage />
+    </>
+  )
+
+}
+
+export default WelcomePage;
