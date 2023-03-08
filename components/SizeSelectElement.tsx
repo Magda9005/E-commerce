dfe83e286c2241ff004fe5cca298651cf24379c7
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-08 17:43:31 +0100

    changing size props to variant

--- components/SizeSelectElement.tsx
+++ components/SizeSelectElement.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 interface Props {
     variants: string[];
-    size: string;
+    variant: string;
     onChange: (value: string) => void
 }
 
-export default function SizeSelectElement({ variants, size, onChange }: Props) {
+export default function SizeSelectElement({ variants, variant, onChange }: Props) {
 
     const handleChange = (event: SelectChangeEvent) => {
         onChange(event.target.value as string);
@@ -20,18 +19,14 @@ export default function SizeSelectElement({ variants, size, onChange }: Props) {
     return (
         <Box sx={{ minWidth: 120 }}>
             <FormControl sx={{ marginBottom: '1em', width: 130 }}>
-                <InputLabel id="demo-simple-select-label">Size</InputLabel>
                 <Select
-                    labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    // value={size}
-                    label="Size"
+                    value={variant}
                     onChange={handleChange}
                     sx={{ ':focus': { outline: '1px solid black' } }}
                     required={true}
                 >
-                    {variants && variants.map((variant, index) => <MenuItem value={variant} 
-                    // selected={index === 0 ? true : false}
+                    {variants && variants.map((variant) => <MenuItem value={variant} 
                     >{variant}</MenuItem>
                     )}
                 </Select>
