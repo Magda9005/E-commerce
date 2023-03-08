54616fe73a58b3fcdc8d265f112beddd708a6854
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-08 16:16:33 +0100

    adding context and creating function for adding product to cart

--- components/SingleProductPage.tsx
+++ components/SingleProductPage.tsx
@@ -1,58 +1,158 @@
 import Navbar from "./Navbar";
 import ProductCard from "./ProductCard";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import styles from './SingleProductPage.module.scss';
+import { MyContext } from "./CartContext";
+import Cookies from "js-cookie";
+import { useMutation } from "urql";
 
-const SingleProductPage = ({ data }) => {
-    const product = data.data.product;
+const createCartMutation = `mutation createCart($selectedVariantId:ID!,$selectedQuantity:Int!) {
+    cartCreate(
+      input: {
+        lines: [
+          {
+            quantity: $selectedQuantity
+            merchandiseId: $selectedVariantId
+          }
+        ],
+      }
+    ) {
+      cart {
+        id
+        createdAt
+        updatedAt
+        lines(first: 10) {
+          edges {
+            node {
+              id
+              merchandise {
+                ... on ProductVariant {
+                  id
+                                  product{
+                                      title
+                                  }
+                }
+              }
+            }
+          }
+        }
+        cost {
+          totalAmount {
+            amount
+            currencyCode
+          }
+          subtotalAmount {
+            amount
+            currencyCode
+          }
+        }
+      }
+    }
+  }`
+
+const addProductToCartMutation = `mutation AddProductToCart($cartId:ID!, $selectedQuantity:Int!,$selectedVariantId:ID!){
+    cartLinesAdd(cartId: $cartId, 
+          lines: [
+              {
+                  merchandiseId: $selectedVariantId,      
+                  quantity: $selectedQuantity
+              }
+          ]
+      )
+      {
+      cart {
+        id
+        lines(first:10) {
+          edges {
+            node {
+              id
+              merchandise {
+                ... on ProductVariant {
+                  id
+                }
+              }
+            }
+          }
+        }
+        cost {
+          totalAmount {
+            amount
+            currencyCode
+          }
+          subtotalAmount {
+            amount
+            currencyCode
+          }
+        }
+      }
+    }
+  }
+  `
+
+const SingleProductPage = ({ productData }) => {
+    const product = productData.data.product;
     const quantityOfFirstVariant = product.variants.edges[0].node.quantityAvailable;
     const [selectedVariantId, setSelectedVariantId] = useState(null);
     const [price, setPrice] = useState(product.variants.edges[0].node.price.amount);
     const [availableQuantity, setAvailableQuantity] = useState(quantityOfFirstVariant);
-    const [quantity, setQuantity] = useState(1)
+    const [selectedQuantity, setSelectedQuantity] = useState(1)
     const variants = product.variants.edges.map(variant => {
-      return variant.node.selectedOptions[0].value
+        return variant.node.selectedOptions[0].value
     })
-    // const[size,setSize]=useState(product.variants.edges[0].node.title)
-  
-    // console.log(size)
-    //co zrobic z tym jak ktos da null w produkcie?
-    //tu potrzebowałabym skorzystać z kontekstu 
-    const handleAddProductToCart = () => {
-      console.log(selectedVariantId, quantity)
-  
-    }
-  
-    return (
-      <>
-        <Navbar itemsQuantity={10}/>
-        <div className={styles.container}>
-          <ProductCard img={product.images.edges[0].node.url}
-            productName={product.title}
-            description={product.description}
-            price={price}
-            variants={variants}
-            availableQuantity={availableQuantity}
-            value={quantity}
-            onValueChange={(val) => setQuantity(val)}
-            onClick={handleAddProductToCart}
-            // size={size}
-            handleSelectChange={(value) => {
-              const variants = product.variants.edges;
-              for (let variant of variants) {
-                if (variant.node.selectedOptions[0].value == value) {
-//   setSize(value)
-                  setPrice(variant.node.price.amount);
-                  setAvailableQuantity(variant.node.quantityAvailable);
-                  setSelectedVariantId(variant.node.id)
-                  setQuantity(1);
-                }
-              }
-            }}
-          />
-        </div>
-      </>
-    )
-  }
 
-  export default SingleProductPage;
+    const cartContext = useContext(MyContext);
+    const [createCartResult, createCart] = useMutation(createCartMutation);
+    const [addProductResult, addProduct] = useMutation(addProductToCartMutation)
+    // const { data, fetching, error } = createCartResult;
+
+
+
+    const handleAddProductToCart = () => {
+        //pobieramy z cookies Id koszyka
+        const cartId = Cookies.get('cartId');
+
+        //jeżeli nie ma w cookies koszyka, musimy utworzyc koszyk, pobrac z responsa id koszyka i ustawic jako cookies
+        if (!cartId) {
+            createCart({ selectedVariantId, selectedQuantity })
+                .then(result => Cookies.set('cartId', result.data.cartCreate.cart.id)
+                )
+        }
+        //jeżeli koszyk istnieje w cookies to dodajemy tylko produkt
+        addProduct({ cartId, selectedQuantity, selectedVariantId })
+
+    }
+
+
+    return (
+        <>
+            <Navbar itemsQuantity={cartContext?.totalQuantity} />
+            <div className={styles.container}>
+                <ProductCard img={product.images.edges[0].node.url}
+                    productName={product.title}
+                    description={product.description}
+                    price={price}
+                    variants={variants}
+                    availableQuantity={availableQuantity}
+                    value={selectedQuantity}
+                    onValueChange={(val) => setSelectedQuantity(val)}
+                    onClick={handleAddProductToCart}
+                    // size={size}
+                    handleSelectChange={(value) => {
+                        const variants = product.variants.edges;
+                        for (let variant of variants) {
+                            if (variant.node.selectedOptions[0].value == value) {
+                                //   setSize(value)
+                                setPrice(variant.node.price.amount);
+                                setAvailableQuantity(variant.node.quantityAvailable);
+                                setSelectedVariantId(variant.node.id)
+                                setSelectedQuantity(1);
+                            }
+                        }
+                    }}
+                />
+            </div>
+        </>
+    )
+}
+
+export default SingleProductPage;
