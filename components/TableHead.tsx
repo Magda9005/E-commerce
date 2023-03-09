811e76aecc7dd96486dc61d5374106a74b1b1498
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-09 19:34:37 +0100

    creating TableHead component

---
+++ components/TableHead.tsx
@@ -0,0 +1,16 @@
+import { TableHead, TableRow, TableCell } from "@mui/material"
+import styles from './TableHead.module.scss';
+
+const TableHeadCart = () => (<TableHead>
+    <TableRow>
+        <TableCell></TableCell>
+        <TableCell className={styles.header} align="center">Product</TableCell>
+        <TableCell className={styles.header} align="center">Variant</TableCell>
+        <TableCell className={styles.header} align="center">Quantity</TableCell>
+        <TableCell className={styles.header} align="center">Unit Price</TableCell>
+        <TableCell className={styles.header} align="center">Total Price</TableCell>
+        <TableCell className={styles.header} align="center">Remove</TableCell>
+    </TableRow>
+</TableHead>)
+
+export default TableHeadCart;
