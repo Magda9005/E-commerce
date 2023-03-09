2a9eaa205fc171ebb38a234c952df05ea4df3baa
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-09 19:34:00 +0100

    creating CartLine component

---
+++ components/CartLine.tsx
@@ -0,0 +1,37 @@
+import NumberInput from "./NumberInput";
+import { TableRow, TableCell,TableBody } from "@mui/material";
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import Link from "next/link";
+import Image from "next/image";
+import styles from './CartLine.module.scss';
+
+const CartLine = ({ title, variant, availableQuantity, quantity, onClick, onValueChange, pricePerUnit, pricePerLine,handle,imgUrl }) => {
+
+    return (
+        <TableBody>
+        <TableRow key={title}>
+        <TableCell component="th" scope="row">
+            <Link href={`/products/${handle}`}>
+                <div className={styles.imgContainer}>
+                    <Image src={imgUrl} alt='product' fill={true}/>
+                </div>
+                </Link>
+        </TableCell>
+        <TableCell className={styles.text} align="center">{title}</TableCell>
+        <TableCell className={styles.text} align="center">{variant}</TableCell>
+        <TableCell className={styles.text} align="center"> <NumberInput
+        availableQuantity={availableQuantity}
+        value={quantity}
+        onValueChange={onValueChange}
+        /></TableCell>
+        <TableCell className={styles.text} align="center">{pricePerUnit}</TableCell>
+        <TableCell className={styles.text} align="center">{pricePerLine}</TableCell>
+        <TableCell className={styles.text} align="center"><button className={styles.removeProductButton} onClick={onClick}
+        ><DeleteOutlinedIcon/></button></TableCell>
+        </TableRow>
+        </TableBody>
+    )
+}
+
+
+export default CartLine;
