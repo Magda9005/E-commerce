24c79de1dae8aff914e4beb30ba2d1b3c8a9cd11
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-10 17:38:09 +0100

    adding additional paths

--- codegen.ts
+++ codegen.ts
@@ -1,25 +1,25 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-   overwrite: true,
-   schema: {
-      "https://skin-therapy-6413.myshopify.com/api/2023-01/graphql.json": {
-         headers: {
-            'Content-Type': 'application/json',
-            'X-Shopify-Storefront-Access-Token': `6ed49a8426a76f3ebfce089ee7b70f69`,
-         }
+  overwrite: true,
+  schema: {
+    "https://skin-therapy-6413.myshopify.com/api/2023-01/graphql.json": {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Storefront-Access-Token": `6ed49a8426a76f3ebfce089ee7b70f69`,
       },
-   }, documents: "pages/**/*.tsx",
-   generates: {
-      "./gql/": {
-         preset: "client",
-         plugins: []
-      },
-      "./graphql.schema.json": {
-         plugins: ["introspection"]
-      }
-   }
+    },
+  },
+  documents:["pages/**/*.tsx","queries/**/*.tsx","mutations/**/*.tsx"] ,
+  generates: {
+    "./gql/": {
+      preset: "client",
+      plugins: [],
+    },
+    "./graphql.schema.json": {
+      plugins: ["introspection"],
+    },
+  },
 };
 
 export default config;
