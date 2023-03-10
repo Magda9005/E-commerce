5696ec6b94e82120bdd82f229e3ce2bbe03d8b8b
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-10 17:47:06 +0100

    replacing fetch with URQL

--- pages/categories/[categoryName]/[productName]/index.tsx
+++ pages/categories/[categoryName]/[productName]/index.tsx
@@ -1,130 +1,75 @@
 import SingleProductPage from "../../../../components/SingleProductPage";
+import { GetStaticProps } from "next";
+import { withUrqlClient, initUrqlClient } from "next-urql";
+import {
+  ssrExchange,
+  dedupExchange,
+  cacheExchange,
+  fetchExchange,
+  useQuery,
+} from "urql";
+import { fetchOptions } from "../../../../helperFunctions/fetchOptions";
+import { getSingleProductQuery, getProductsPathsQuery } from "../../../../queries/queries";
+import { createClient } from "urql";
+
+interface Props {
+  productName: string;
+}
+
+const ProductPage = ({ productName }: Props) => {
+  const [res] = useQuery({
+    query: getSingleProductQuery,
+    variables: { productName },
+  });
+
+  return <SingleProductPage productData={res} />;
+};
 
 export const getStaticPaths = async () => {
-    const url = process.env.NEXT_PUBLIC_API
-    const token = process.env.NEXT_PUBLIC_API_TOKEN;
-    const query = `{
-        products(first: 30) {
-         pageInfo {
-          hasNextPage
-          endCursor
-        }
-          edges {
-            node {
-              id
-              title
-              handle
-              description
-              variants(first: 1) {
-          edges {
-            node {
-              price {
-                amount
-              }
-            }
-          }
-        }
-              images(first:1){
-          edges {
-            node{
-              url
-            }
-          }
-        }
-							collections(first: 3) {
-          edges {
-            node {
-              handle
-            }
-          }
-        }
-            }
-          }
-        
-        }
-      }`
-    const res = await fetch(url,
-        {
-            method: 'POST',
-            headers: {
-                "Content-type": "application/json",
-                "X-Shopify-Storefront-Access-Token": token
-            },
-            body: JSON.stringify({
-                query: query
-            })
-        })
-    const data = await res.json()
-    const products = data.data.products.edges
-    const paths = products.map(product => {
-        return {
-            params: { categoryName: product.node.collections.edges[0].node.handle, productName: product.node.handle }
-        }
-    })
+  const client = createClient({ ...fetchOptions });
 
+  const data = await client.query(getProductsPathsQuery,{}).toPromise();
 
+  const products = data.data.products.edges;
+  const paths = products.map((product) => {
     return {
-        paths: paths,
-        fallback: false
-    }
+      params: {
+        categoryName: product.node.collections.edges[0].node.handle,
+        productName: product.node.handle,
+      },
+    };
+  });
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps:GetStaticProps=async(context)=> {
+  const ssrCache = ssrExchange({ isClient: false });
+  const client = initUrqlClient(
+    {
+      url: fetchOptions.url,
+      fetchOptions: fetchOptions.fetchOptions,
+      exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
+    },
+    false
+  );
+
+  const productName = context?.params?.productName as string;
+  const variables = { productName };
+
+  await client?.query(getSingleProductQuery, variables).toPromise();
+
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+      productName,
+    },
+    revalidate: 600,
+  };
 }
 
-const ProductPage = ({ data }) => (<SingleProductPage productData={data} />)
-
-export async function getStaticProps(context) {
-    const productName = context.params.productName;
-    const url = process.env.NEXT_PUBLIC_API
-    const token = process.env.NEXT_PUBLIC_API_TOKEN;
-    const variables = { productName };
-    const query = `query GetSingleProduct($productName:String!){
-          product(handle: $productName){
-                 id
-                 title
-                 handle
-                 description
-                 variants(first: 3) {
-             edges {
-               node {
-                id
-                quantityAvailable
-                              selectedOptions {
-              value
-            }
-                 price {
-                   amount
-                 }
-               }
-             }
-           }
-                 images(first:1){
-             edges {
-               node{
-                 url
-               }
-             }
-           }
-               }
-             }
-           `
-    const res = await fetch(url,
-        {
-            method: 'POST',
-            headers: {
-                "Content-type": "application/json",
-                "X-Shopify-Storefront-Access-Token": token
-            },
-            body: JSON.stringify({
-                query,
-                variables
-            })
-        })
-    const data = await res.json();
-
-    return {
-        props: {
-            data
-        },
-    }
-}
-
-export default ProductPage;
+export default withUrqlClient((ssr) => ({ ...fetchOptions }))(ProductPage);
--- pages/categories/[categoryName]/index.tsx
+++ pages/categories/[categoryName]/index.tsx
@@ -1,107 +1,86 @@
 import { useState, useContext } from "react";
+import { GetStaticProps } from "next";
 import ListElement from "../../../components/ListElement";
 import Navbar from "../../../components/Navbar";
-import styles from '../categoryName.module.scss';
+import styles from "../categoryName.module.scss";
 import { MyContext } from "../../../components/CartContext";
+import { withUrqlClient, initUrqlClient } from "next-urql";
+import {
+  ssrExchange,
+  dedupExchange,
+  cacheExchange,
+  fetchExchange,
+  useQuery,
+} from "urql";
+import { fetchOptions } from "../../../helperFunctions/fetchOptions";
+import { getProductsByCollectionQuery } from "../../../queries/queries";
 
+interface Props {
+  categoryName: string
+}
+
+const ProductsListPerCategory = ({ categoryName }: Props) => {
+  const [data] = useQuery({
+    query: getProductsByCollectionQuery,
+    variables: { categoryName },
+  });
+  const context = useContext(MyContext);
+
+  const products = data?.data?.collection?.products.edges ;
+  const [productsList, setProductsList] = useState(products);
+
+  return (
+    <>
+      <Navbar itemsQuantity={context?.totalQuantity} />
+      <div className={styles.container}>
+        {productsList && productsList.map((product) => (
+          <ListElement
+            key={product.node.id}
+            img={product.node.images.edges[0].node.url}
+            productName={product.node.title}
+            price={product.node.variants.edges[0].node.price.amount}
+            description={product.node.description}
+            handle={`${categoryName}/${product.node.handle}`}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssrCache = ssrExchange({ isClient: false });
+  const client = initUrqlClient(
+    {
+      url: fetchOptions.url,
+      fetchOptions: fetchOptions.fetchOptions,
+      exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
+    },
+    false
+  );
+  const categoryName = context?.params?.categoryName as string;
+  const variables = { categoryName };
+
+  await client?.query(getProductsByCollectionQuery, variables).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+      categoryName,
+    },
+    revalidate: 600,
+  };
+}
 
 export const getStaticPaths = async () => {
-    return {
-        paths: [
-            { params: { categoryName: "skin-care" } },
-            { params: { categoryName: "hair-care" } },
-            { params: { categoryName: "makeup" } },
-        ],
-        fallback: false
-    }
-}
+  return {
+    paths: [
+      { params: { categoryName: "skin-care" } },
+      { params: { categoryName: "hair-care" } },
+      { params: { categoryName: "makeup" } },
+    ],
+    fallback: false,
+  };
+};
 
-const ProductsListPerCategory = ({ data, categoryName }) => {
-
-    const products = data.data.collection.products.edges
-    const [productsList, setProductsList] = useState(products)
-    const context = useContext(MyContext);
-
-    return (
-        <>
-            <Navbar itemsQuantity={context?.totalQuantity} />
-            <div className={styles.container}>
-                {productsList.map(product => <ListElement
-                    key={product.node.id}
-                    img={product.node.images.edges[0].node.url}
-                    productName={product.node.title}
-                    price={product.node.variants.edges[0].node.price.amount}
-                    description={product.node.description}
-                    handle={`${categoryName}/${product.node.handle}`}
-                />)}
-            </div>
-        </>
-    )
-
-}
-
-export async function getStaticProps(context) {
-    const categoryName = context.params.categoryName
-    const url = process.env.NEXT_PUBLIC_API
-    const token = process.env.NEXT_PUBLIC_API_TOKEN;
-    const variables = { categoryName };
-    const query = `query GetProductsByCollection($categoryName:String!){
-        collection(handle:$categoryName){
-       products(first:3){
-          pageInfo {
-           hasNextPage
-           endCursor
-         }
-           edges {
-             node {
-               id
-               title
-                           handle
-               description
-               variants(first: 1) {
-           edges {
-             node {
-               price {
-                 amount
-               }
-             }
-           }
-         }
-               images(first:1){
-           edges {
-             node{
-               url
-             }
-           }
-         }
-             }
-           }
-         
-         }
-       }
-       
-    }`
-    const res = await fetch(url,
-        {
-            method: 'POST',
-            headers: {
-                "Content-type": "application/json",
-                "X-Shopify-Storefront-Access-Token": token
-            },
-            body: JSON.stringify({
-                query,
-                variables
-            })
-        })
-    const data = await res.json();
-
-    return {
-        props: {
-            data,
-            categoryName
-        },
-    }
-}
-
-
-export default ProductsListPerCategory;
+export default withUrqlClient((ssr) => ({ ...fetchOptions }))(ProductsListPerCategory);
