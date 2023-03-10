bbb5b4442682e1dc862bba647b7999329356931d
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-10 17:40:41 +0100

    creating directory for graphql queries

---
+++ queries/queries.tsx
@@ -0,0 +1,199 @@
+import { graphql } from "../gql/gql";
+
+export const getSingleProductQuery = graphql(/* GraphQL */ `
+  query GetSingleProduct($productName: String!) {
+    product(handle: $productName) {
+      id
+      title
+      handle
+      description
+      variants(first: 3) {
+        edges {
+          node {
+            id
+            quantityAvailable
+            selectedOptions {
+              value
+            }
+            price {
+              amount
+            }
+          }
+        }
+      }
+      images(first: 1) {
+        edges {
+          node {
+            url
+          }
+        }
+      }
+    }
+  }
+`);
+
+export const getProductsPathsQuery = graphql(/* GraphQL */ `
+  query GetProductsPaths {
+    products(first: 30) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      edges {
+        node {
+          id
+          title
+          handle
+          description
+          variants(first: 1) {
+            edges {
+              node {
+                price {
+                  amount
+                }
+              }
+            }
+          }
+          images(first: 1) {
+            edges {
+              node {
+                url
+              }
+            }
+          }
+          collections(first: 3) {
+            edges {
+              node {
+                handle
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`);
+
+export const getProductsByCollectionQuery =graphql(/* GraphQL */ `query GetProductsByCollection($categoryName:String!){
+  collection(handle:$categoryName){
+ products(first:3){
+    pageInfo {
+     hasNextPage
+     endCursor
+   }
+     edges {
+       node {
+         id
+         title
+                     handle
+         description
+         variants(first: 1) {
+     edges {
+       node {
+         price {
+           amount
+         }
+       }
+     }
+   }
+         images(first:1){
+     edges {
+       node{
+         url
+       }
+     }
+   }
+       }
+     }
+   
+   }
+ }
+ 
+}`
+);
+
+export const getCart =graphql(/* GraphQL */ `query GetCart($cartId:ID!){
+  cart(id: $cartId) {
+  id
+cost{
+totalAmount {
+amount
+currencyCode
+}
+}
+      totalQuantity
+  createdAt
+  updatedAt
+  lines(first: 10) {
+    edges {
+      node {
+        id
+        cost{
+          totalAmount{
+            amount
+            currencyCode
+          }
+        }
+        merchandise {
+            ... on ProductVariant {
+              id
+              quantityAvailable
+                              product {
+                                  title
+                                  handle
+                                  images(first:1){
+                                    edges {
+                                      node{
+                                        url
+                                      }
+                                    }
+                                  }
+                              }
+                              title
+                              price {
+                                amount
+                              }
+            }
+          }
+        quantity
+      }
+    }
+  }
+}
+}
+        `);
+
+export const getFirst3productsQuery =graphql(/* GraphQL */ `query GetFirst3Products {
+  products(first: 3) {
+   pageInfo {
+    hasNextPage
+    endCursor
+  }
+    edges {
+      node {
+        id
+        title
+        handle
+        description
+        variants(first: 1) {
+    edges {
+      node {
+        price {
+          amount
+        }
+      }
+    }
+  }
+        images(first:1){
+    edges {
+      node{
+        url
+      }
+    }
+  }
+      }
+    }
+  
+  }
+}
+`);
