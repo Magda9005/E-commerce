16bec7b167a24b4292d667928b537531fbdd9f62
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-11 10:23:41 +0100

    creating Error component

---
+++ components/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+
+interface Props {
+    errorMessage: string;
+}
+
+export const ErrorMessage = ({ errorMessage }: Props) => {
+    return (
+        <Alert severity="error">{errorMessage}</Alert>
+    );
+}
+
+export default ErrorMessage;
