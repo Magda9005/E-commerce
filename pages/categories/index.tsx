b00a4fb341ecf1c77a9690a6a4ab395c0a894389
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2023-03-15 18:45:00 +0100

    wrapping component in urql client

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -4,6 +4,8 @@ import styles from "./index.module.scss";
 import Link from "next/link";
 import useCart from "../../hooks/useCart";
 import { categories } from "../../helperFunctionsAndConstants/constants";
+import { withUrqlClient } from "next-urql";
+import { fetchOptions } from "../../helperFunctionsAndConstants/fetchOptions";
 
 const Categories = () => {
   const context = useCart();
@@ -26,4 +28,5 @@ const Categories = () => {
   );
 };
 
-export default Categories;
+export default withUrqlClient((ssr) => ({ ...fetchOptions }))(Categories);
+
